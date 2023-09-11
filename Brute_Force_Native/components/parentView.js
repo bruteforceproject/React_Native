131221849920706561279573773d61e97a367766
JavierGarciaJr <76506163+JavierGarciaJr@users.noreply.github.com>
2023-09-10 18:43:35 -0700

    ParentViewNative

---
+++ Brute_Force_Native/components/parentView.js
@@ -0,0 +1,108 @@
+
+import React, { useEffect } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { Image } from 'react-native';
+
+const ParentView = ({ navigation }) => {
+ 
+  useEffect(() => {
+    navigation.setOptions({
+      title: 'Parent Portal',
+      headerTitleAlign: 'center'
+    });
+  }, [navigation]);
+  
+  
+  const getButtonStyle = (text, id) => {
+    const textLength = text.length;
+    let styleName;
+
+    if (id === 'textLength') {
+      styleName = textLength <= 10 ? 'short' : textLength <= 20 ? 'medium' : 'long';
+    } else {
+      styleName = textLength <= 15 ? 'short' : textLength <= 25 ? 'medium' : 'long';
+    }
+
+    return styles[styleName];
+  };
+
+  return (
+    <View style={styles.container}>
+      <Image source={require("../assets/TransparentPELogo.png")} style={{ width: 200, height: 200, marginTop: -150 }}/>
+      {/* <Text numberOfLines={1} style={styles.PortalEdHeader}>Portal Ed</Text> */}
+      <Text style={styles.GuardianName}>Parent Name</Text>
+
+      <TouchableOpacity style={[styles.buttonContainer, getButtonStyle('Student Name', 'textLength')]}>
+        <View style={styles.buttonContent}>
+          <Text numberOfLines={1} style={styles.studentName}>Student Name</Text>
+          <Text style={styles.alertText}>2 Unacknowledged Alerts!</Text>
+        </View>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={[styles.buttonContainer, getButtonStyle('Another Student', 'textLength2')]}>
+        <View style={styles.buttonContent}>
+          <Text numberOfLines={1} style={styles.studentName}>Another Student</Text>
+          <Text style={styles.studentId}>0 Unacknowledged Alerts</Text>
+        </View>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  PortalEdHeader: {
+    fontSize: 36,
+    marginBottom: 25,
+  },
+  GuardianName: {
+    fontSize: 22,
+    marginBottom: 20,
+  },
+  studentName: {
+    fontSize: 20, // Add this line to set the font size
+    // ... (any other styles you have or want to add)
+  },
+  alertText: {
+    fontSize: 16, // Add this line to set the font size
+    color: 'red',
+    // ... (any other styles you have or want to add)
+  },
+  buttonContainer: {
+    backgroundColor: '#D9D9D9', // or any other color you like
+    padding: 10, // Reduced padding
+    borderRadius: 5,
+    margin: 5,
+    width: 267,  // Fixed width
+    height: 120,  // Fixed height
+    justifyContent: 'center', // Centers content vertically
+    alignItems: 'center', // Centers content horizontally
+  },
+  buttonContent: {
+    flexDirection: 'column',  // Set direction to column for vertical arrangement
+    alignItems: 'center', // Align items to the center
+  },
+  short: {
+    backgroundColor: '#D9D9D9',
+  },
+  medium: {
+    backgroundColor: '#D9D9D9',
+  },
+  long: {
+    backgroundColor: '#D9D9D9',
+  },
+  studentId: {
+    marginLeft: 10,
+    fontSize: 16,
+  },
+  studentId2: {
+    fontSize: 16,
+    marginLeft: 10,
+  },
+});
+
+export default ParentView;
---
+++ Brute_Force_Native/components/text.js
@@ -0,0 +1,5 @@
+const text = () => {
+    return(
+        <h1> Hi there!</h1>
+    );
+}
