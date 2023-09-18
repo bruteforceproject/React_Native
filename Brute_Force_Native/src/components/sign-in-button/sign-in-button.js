7c6dbe6434409df18828eb67ed6154d49cf53192
BiggyBank <angelac1357@gmail.com>
2023-09-18 07:18:22 -0700

    Sign In Screen Updated

--- Brute_Force_Native/src/components/sign-in-button/sign-in-button.js
+++ Brute_Force_Native/src/components/sign-in-button/sign-in-button.js
@@ -1,17 +1,37 @@
 import React from 'react';
-import { View, Text, StyleSheet} from 'react-native';
+import { View, Text, StyleSheet, Pressable} from 'react-native';
 
-const SignInButton = () => {
+const SignInButton = ({ onPress, text, type='primary' }) => {
     return (
-        <View style={styles.field}>
-            <Text style={styles.text}> Click here </Text>
-        </View>
+        <Pressable onPress={onPress} style={[styles.field, styles[`field_${type}`]]}>
+            <Text style={[styles.text, styles[`text_${type}`]]}> {text} </Text>
+        </Pressable>
     );
 };
 
 const styles =  StyleSheet.create({
-    field: {},
-    text: {}
+    field: {
+        width: '80%',
+        alignItems: 'center',
+        borderRadius: 10,
+        paddingVertical: 10,
+        paddingHorizontal: 10,
+        marginVertical: 10,
+    },
+    field_primary: {
+        backgroundColor: '#3B71F3',
+    },
+    field_tertiary: {
+        
+    },
+    text: {
+        fontWeight: 'bold',
+        color: 'white'
+    },
+    text_tertiary: {
+        fontWeight: 'light',
+        color: 'gray'
+    },
 });
 
 export default SignInButton
--- Brute_Force_Native/src/screens/sign-in/sign-in.js
+++ Brute_Force_Native/src/screens/sign-in/sign-in.js
@@ -1,5 +1,5 @@
 import React , {useState} from 'react';
-import { View, Text , Image, StyleSheet, useWindowDimensions} from 'react-native';
+import { View, Text , Image, StyleSheet, useWindowDimensions, ScrollView} from 'react-native';
 import Logo from '../../../assets/TransparentPELogo.png';
 import FieldInput from '../../components/field-inputs/field-input';
 import SignInButton from '../../components/sign-in-button/sign-in-button';
@@ -11,7 +11,16 @@ const SignInScreen = () => {
 
     const {height} = useWindowDimensions();
 
+    const onSignInPressed = () => {
+        console.warn('Sign In');
+    }
+
+    const onForgotPressed = () => {
+        console.warn('Forgot');
+    }
+
     return (
+        <ScrollView showsHorizontalScrollIndicator={false}>
         <View style={styles.root}>
             <Image source={Logo} 
                 style={[styles.logo, {height: height * 0.3}]} 
@@ -30,15 +39,26 @@ const SignInScreen = () => {
                 secureTextEntry
                 />
 
-            <SignInButton/>
+            <SignInButton 
+                text='Sign In' 
+                onPress={onSignInPressed}
+                />
+
+            <SignInButton 
+                text='Forgot Account ID or password?' 
+                onPress={onForgotPressed}
+                type='tertiary'
+                />
+
         </View>
+        </ScrollView>
     );
 };
 
 const styles = StyleSheet.create({
     root: {
         alignItems: 'center',
-        padding: 20,
+        paddingVertical: 100,
     },
 
     logo: {
