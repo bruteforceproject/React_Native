6463451c724e3327b92b29308cfa7c6151d10124
ArielManalo14 <91509709+ArielManalo14@users.noreply.github.com>
2023-10-15 15:10:29 -0700

    BF-200
    connecting to mongodb

---
+++ databaseConnection/server.js
@@ -0,0 +1,39 @@
+//testing purposes
+const express = require("express");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
+const app = express();
+
+let db;
+const url = `mongodb+srv://Ramez:GHipRjutHHid5CG7@portaledcluster.x6u4jx9.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+app.use(express.json());
+
+
+async function startServer() {
+
+  try {
+    await client.connect();
+    
+    console.log("Connected to MongoDB");
+
+    const database = client.db("PortedEd");
+    const studentCollection = database.collection("Student");
+    const parentCollection = database.collection("Parent");
+    const teacherCollection = database.collection("Teacher");
+
+
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+  } finally {
+    //await client.close();
+  }
+}
+// Call the startServer function to start the server
+startServer();
